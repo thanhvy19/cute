8e4d9b4892ec823932c9296ec8bd832e69288440
theyluvuyn <123577043+theyluvuyn@users.noreply.github.com>
2023-01-27 21:59:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    titleWeb: "Gửi em",
+    titleWeb: "theluvuyn",
 
     background: "background.jpg",
 
-    min1: "min1.jfif",
-    min2: "min2.jpg",
-    min3: "max3.jpg",
-    min4: "max4.webp",
-    min5: "min5.jpg",
-    min6: "min6.jpg",
+    min1: "img1.jpg",
+    min2: "img2.jpg",
+    min3: "img3.jpg",
+    min4: "img4.jpg",
+    min5: "img5.jpg",
+    min6: "img6.jpg",
 
-    max1: "max1.jpg",
-    max2: "max2.jpg",
-    max3: "max3.jpg",
-    max4: "max4.webp",
-    max5: "max5.png",
-    max6: "max6.jpg",
+    max1: "img7.jpg",
+    max2: "img8.jpg",
+    max3: "img9.jpg",
+    max4: "img10.jpg",
+    max5: "img11.jpg",
+    max6: "img12.jpg",
 }
